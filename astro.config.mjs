35d1d2495ec960fbe671dfa5c43d865854107344
romariojs94 <romariojdosantos@gmail.com>
2024-03-15 00:25:41 -0300

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://romariojs94.github.io',
+	base: '/social-links-profile',
   integrations: [
 		tailwind({
 			nesting: true,
